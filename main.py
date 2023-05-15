b29393be9137566f8e85d5759a89f08bbd10f26a
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-15 15:59:05 -0300

    Fix change of tests

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 i = input("Digite sua idade atual: ")
 i = int(i)
 if i < 0:
-    print ("impossível!")
+    print ("Impossível!")
 elif i < 18:
-    print("não precisa se alistar.")
+    print("Não precisa se alistar.")
 elif i == 18 or i < 65:
     print("Não esqueça de votar na próxima eleição")
 elif i > 65: 
     print("Vá descansar.")
 else:
-    print("eita!")
+    print("Eita!")
